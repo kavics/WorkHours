a6703370a22ee5bfa567b46b0d94782b16d21855
kavics <gyeby5@gmail.com>
2020-04-25 00:52:21 +0200

    Total statistics in ta textbox. (#4)

---
+++ src/WorkHours/Statistics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkHours
+{
+    public class Statistics
+    {
+        public static TimeSpan ExpectedWorkTimePerWorkDay { get; set; } = TimeSpan.FromHours(6.5d);
+        public List<WorkDay> WorkDays { get; set; }
+
+        public TimeSpan TotalExpectedWorkTime { get; private set; }
+        public TimeSpan TotalWorkTime { get; private set; }
+        public TimeSpan Diff { get; private set; }
+        public double Rate { get; private set; }
+
+        public void Compute()
+        {
+            TimeSpan expected = TimeSpan.Zero;
+            TimeSpan work = TimeSpan.Zero;
+            foreach (var workDay in WorkDays)
+            {
+                if (!workDay.IsHoliday)
+                    expected += ExpectedWorkTimePerWorkDay;
+                work += workDay.WorkHours;
+            }
+
+            TotalExpectedWorkTime = expected;
+            TotalWorkTime = work;
+            Diff = work - expected;
+            Rate = work.TotalHours / expected.TotalHours;
+        }
+    }
+}
